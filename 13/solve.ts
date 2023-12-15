fc7b3627b26201f650289d6ff80d31560a75cb1f
Jakub <2474411+vetvicka@users.noreply.github.com>
2023-12-15 15:09:53 +0100

    13 - part2 solved

--- 13/solve.ts
+++ 13/solve.ts
@@ -2,10 +2,6 @@ import { parseFile, flip } from './parse'
 
 const parsed = parseFile('input.txt');
 
-const debug = parsed[2];
-
-console.log(debug);
-
 function findReflections(map: string[]) {
     const edge = map[0];
     const candidates: number[] = [];
@@ -48,4 +44,35 @@ function bothOrientations(data: { original: string[], flipped: string[] }) {
     return both.reduce((acc, value) => acc + value, 0);
 }
 
-console.log("part1: ", parsed.map(bothOrientations).reduce((acc, value) => acc + value, 0));
+function bothOrientations2(data: { original: string[], flipped: string[] }) {
+    const original = bothDirections(data.original);
+    const flipped = bothDirections(data.flipped);
+    return [...flipped, ...[...original].map(value => value * 100)]
+}
+
+const part1Results = parsed.map(bothOrientations);
+console.log("part1: ", part1Results.reduce((acc, value) => acc + value, 0));
+
+function findSmudge(map: string[], mapIndex = 0) {
+    for (let i = 0; i < map.length; i++) {
+        for (let j = 0; j < map[i].length; j++) {
+            const originalElement = map[i][j];
+            const originalLine = map[i];
+            const newElement = originalElement === '#' ? '.' : '#';
+            map[i] = map[i].substring(0, j) + newElement + map[i].substring(j + 1);
+            const exampleDataInner = {
+                original: map,
+                flipped: flip(map),
+            }
+            const changedCharResult = bothOrientations2(exampleDataInner);
+            const newResult = changedCharResult.find((el) => part1Results[mapIndex] !== el)
+            map[i] = originalLine;
+            if (newResult) {
+                return newResult
+            }
+        }    
+    }
+}
+
+const part2Results = parsed.map((data, i) => findSmudge(data.original, i));
+console.log("part2:", part2Results.reduce((acc, value) => acc + value, 0))
