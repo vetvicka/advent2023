57ff6d1e54edf133c875e18cc859b8eb782b83b6
Jakub <2474411+vetvicka@users.noreply.github.com>
2024-01-09 08:21:19 +0100

    19 - part2 solved

--- 19/solve.ts
+++ 19/solve.ts
@@ -33,4 +33,73 @@ function solve() {
     console.log("part1: ", result)
 }
 
+function invertRule(rule: Rule): Rule {
+    switch (rule.operator) {
+        case "<":
+            return {
+                ...rule,
+                operator: ">",
+                value: rule.value - 1,
+            }
+        case ">":
+            return {
+                ...rule,
+                operator: "<",
+                value: rule.value + 1,
+            }
+    }
+    throw new Error("Cannot invert redirect rule")
+}
+
+function instructionChains(instructions: { [key: string]: Rule[] }, currentKey: string = "in", chain: Rule[] = []) {
+    const chains: Rule[][] = [];
+    const rules = instructions[currentKey];
+    rules.forEach(rule => {
+        if (rule.operator === "redirect") {
+            if (["A", "R"].includes(rule.target)) {
+                chains.push([...chain, rule])
+            } else {
+                chains.push(...instructionChains(instructions, rule.target, [...chain, rule]))
+            }
+        } else if(["A", "R"].includes(rule.target)){
+            chains.push([...chain, rule])
+        } else {
+            chains.push(...instructionChains(instructions, rule.target, [...chain, rule]))
+        }
+        if (rule.operator !== "redirect") {
+            chain.push(invertRule(rule))
+        }
+    })
+    return chains;
+}
+
+function solve2() {
+    const res = instructionChains(parsed.instructions)
+        .filter(chain => chain[chain.length - 1].target === "A")
+    const ranges = res.map(chain => {
+        const propertyRanges = {
+            x: [1, 4000],
+            m: [1, 4000],
+            a: [1, 4000],
+            s: [1, 4000],
+        }
+        chain.filter(rule => rule.operator !== "redirect")
+            .forEach(rule => {
+                if (rule.operator === "<") {
+                    propertyRanges[rule.property][1] = Math.min(propertyRanges[rule.property][1], rule.value - 1)
+                }
+                if (rule.operator === ">") {
+                    propertyRanges[rule.property][0] = Math.max(propertyRanges[rule.property][0], rule.value + 1)
+                }
+            })
+        return propertyRanges;
+    })
+    const product = ranges.map(({x,m,a,s}) => {
+        return (x[1] - x[0] + 1) * (m[1] - m[0] + 1) * (a[1] - a[0] + 1) * (s[1] - s[0] + 1);
+    })
+    console.log("part2: ", product.reduce((acc, x) => acc + x, 0))
+}
+
 solve()
+
+solve2();
